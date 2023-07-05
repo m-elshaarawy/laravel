306f712b051a06301d6eb32e633ce291e5b862db
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-07-05 23:17:07 +0300

    Controllers L3

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use GuzzleHttp\Psr7\Request;
 #use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,16 +24,17 @@ Route::get('/', function () {
 // Route::post('users', function(Request $request){
 //     return $request; //view('users');
 // });
-
-Route::get('users',function(){
-    //$name ='mohamed';
-    return view('users');//compact('name'),,["name"=> "mohamed"]
-});
+#------------------------------------------------------------------
+// Route::get('users',function(){
+//     //$name ='mohamed';
+//     return view('users');//compact('name'),,["name"=> "mohamed"]
+// });
+#------------------------------------------------------------------
 // Route::get('posts',function(){
 //     return view('posts');
 // });
 
-
+#------------------------------------------------------------------
 Route::controller(PostController::class)->group(function(){
 
     Route::get('posts','index');
@@ -42,3 +44,5 @@ Route::controller(PostController::class)->group(function(){
 
 // Route::get('posts',[PostController::class,'index']);
 // Route::get('posts/create',[PostController::class,'createPost']);
+#--------------------------------------------------------------------
+Route::resource('users',UserController::class);
