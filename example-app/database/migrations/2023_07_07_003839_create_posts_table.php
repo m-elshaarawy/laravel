5ac659f09c82da18c824de9dec88c1a4c40653f6
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-07-07 22:13:44 +0300

    Eloquent Models L 5 softDelete

--- example-app/database/migrations/2023_07_07_003839_create_posts_table.php
+++ example-app/database/migrations/2023_07_07_003839_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title',20);
             $table->string('body');
+            $table->softDeletes();//<---=====--->
             $table->timestamps();
         });
     }
